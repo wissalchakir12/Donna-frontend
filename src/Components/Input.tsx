1fcfc2b4081ecf04c3b061b6e1f357ceb51df323
Thami <thamielmoumeni@gmail.com>
2024-09-26 00:06:07 +0100

    Commande (espace entre les champs non regle)

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -4,12 +4,14 @@ import './Input.module.css';
 type Props = {
   width : number;
   type : string;
+  className ? : string ;
+  placeholder : string;
 };
 
-const Input:React.FC<Props> = ({width, type}) => {
+const Input:React.FC<Props> = ({width, type,className,placeholder}) => {
   return (
     <div className='box'>
-      <input type={type} style={{width: `${width}mm`}} />
+      <input placeholder={placeholder} type={type} style={{width: `${width}mm`}} className={className} />
     </div>
   )
 }
--- src/Pages/Commande.tsx
+++ src/Pages/Commande.tsx
@@ -4,29 +4,32 @@ import Input from '../Components/Input';
 
 export const Commande = () => {
   return (
-    <div className='main'>
-        <div className='text'>
-          <div>CONFIRMEZ <img style={{overflow:'hidden', width:'14rem'}} src="src/assets/rays.png"/></div> VOTRE COMMANDE
-        </div>
-        <div className='form'>
-          <div className="field">
-            <p>Nom</p>
-            <p>Prenom</p>
-            <p>Telephone</p>
-            <p>Mail</p>
-            <p>Adresse</p>
+    <>
+      
+      <div className='main'>
+        <img src="src/assets/leaf1.png" className='leaf1'/>
+        <img src="src/assets/leaf2.png" className='leaf2'/>
+        <img src="src/assets/leaf3.png" className='leaf3'/>
+          <div className='text'>
+            <div>CONFIRMEZ <img style={{overflow:'hidden', width:'14rem'}} src="src/assets/rays.png"/></div> VOTRE COMMANDE
           </div>
-          <div className="inputs">
-            <Input width={70} type='text'/>
-            <Input width={70} type='text'/>
-            <Input width={50} type='phone'/>
-            <Input width={80} type='mail'/>
-            <Input width={110} type='text'/>
+          <div className='form'>
+            <div className="field">
+              <p>Nom Complet</p>
+              <p>Telephone</p>
+              <p>Mail</p>
+              <p>Adresse</p>
+            </div>
+            <div className="inputs">
+              <Input placeholder='Nom Complet' width={70} type='text' className='name'/>
+              <Input placeholder='Telephone' width={50} type='phone' className='phone'/>
+              <Input placeholder='Email' width={80} type='mail' className='mail'/>
+              <Input placeholder='Adresse' width={80} type='text' className='adresse'/>
+            </div>
           </div>
-        </div>
-        <p className='total'>Total :</p>
-        <button className='confirm'>Confirmer</button>
-    </div>
-    
+          <p className='total'>Total : </p>
+          <button className='confirm'>Confirmer</button>
+      </div>
+    </>
   )
 }
